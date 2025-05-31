3a12c0e87b9e9da8b7376952012d2b669db37ad6
Renat03 <grsrenat@gmail.com>
2025-05-31 18:37:35 +0300

    Add Search Functionality

--- go2web.py
+++ go2web.py
@@ -3,6 +3,9 @@ import socket
 from urllib.parse import urlparse
 import ssl
 import html2text
+from bs4 import BeautifulSoup
+from urllib.parse import quote_plus
+import webbrowser
 
 
 def convert_html_to_text(html_content):
@@ -50,6 +53,31 @@ def fetch_web_content(target_url):
     _, _, body = response.partition(b'\r\n\r\n')
     return 'text/html', body.decode('utf-8', errors='ignore')
 
+
+def perform_search(search_query):
+    search_url = f"http://www.bing.com/search?q={quote_plus(search_query)}"
+    content_type, html_data = fetch_web_content(search_url)
+
+    soup = BeautifulSoup(html_data, 'html.parser')
+    results = []
+
+    for item in soup.find_all('li', class_='b_algo'):
+        link = item.find('a')
+        if link:
+            results.append({
+                'title': link.get_text(strip=True),
+                'link': link['href']
+            })
+            if len(results) >= 10:
+                break
+
+    print(f"Top {len(results)} results:")
+    for idx, item in enumerate(results, 1):
+        print(f"{idx}. {item['title']}\n   {item['link']}")
+
+    return results
+
+
 def execute_cli():
     arg_parser = argparse.ArgumentParser(description='go2web - Web client utility')
     arg_parser.add_argument('-u', '--url', help='make an HTTP request to the specified URL and print the response')
@@ -64,6 +92,10 @@ def execute_cli():
     if arguments.url:
         content_type, content = fetch_web_content(arguments.url)
         print(format_content(content_type, content))
+
+    elif arguments.search:
+        query = ' '.join(arguments.search)
+        perform_search(query)
 
 if __name__ == '__main__':
     execute_cli()
